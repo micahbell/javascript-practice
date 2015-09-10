cb0188a4a295eaaf9d31332c48e93c64800a6393
Micah Bell <micahmbell@gmail.com>
2015-09-10 11:49:14 -0600

    00_functions.

--- 00_functions/src.js
+++ 00_functions/src.js
@@ -1,10 +1,43 @@
 module.exports = {
 
   helloWorld: function(){
-    // fill me in
+    var string = 'Hello World';
+    return string;
   },
 
   hello: function(string){
-    // fill me in
+    var result = 'Hello ' + string;
+    return result;
+  },
+
+  shout: function(string) {
+    var result = 'Hello ' + string.toUpperCase();
+    return result;
+  },
+
+  whisper: function(string) {
+    var result = 'Hello ' + string.toLowerCase();
+    return result;
+  },
+
+  separate: function(string) {
+    var result = string.split('');
+    return result;
+  },
+
+  reverseJoin: function(array) {
+    var result = array.reverse().join();
+    return result;
+  },
+
+  reverseJoinCompact: function(array) {
+    var result = array.reverse().join('');
+    return result;
+  },
+
+  backwardsDay: function(string) {
+    var reverse = string.split('').reverse().join('');
+    var result = 'Hello ' + reverse;
+    return result;
   }
 }
