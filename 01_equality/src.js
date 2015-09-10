86e075b736e28056a28956d9b7abf33d5bd75de5
Micah Bell <micahmbell@gmail.com>
2015-09-10 12:47:23 -0600

    01_equality

--- 01_equality/src.js
+++ 01_equality/src.js
@@ -1,5 +1,53 @@
 module.exports = {
   sameLength: function(string1, string2) {
-    // your code here:
+    if(string1.length === string2.length) {
+      var result = true;
+    } else if(string1.length != string2.length) {
+      var result = false;
+    }
+    return result;
+  },
+
+  firstGreater: function(x, y) {
+    if(x > y) {
+      var result = true;
+    } else if(x <= y) {
+      var result = false;
+    }
+    return result;
+  },
+
+  firstGreaterOrEqualTo: function(x, y) {
+    if(x >= y) {
+      var result = true;
+    } else if (x < y) {
+      var result = false;
+    }
+    return result;
+  },
+
+  firstLengthGreater: function(x, y) {
+    if(x.length > y.length) {
+      var result = true;
+    } else if(x.length <= y.length) {
+      var result = false;
+    }
+    return result;
+  },
+
+  secondGreater: function(x, y) {
+    if(x < y) {
+      var result = true;
+    } else if(x >= y) {
+      var result = false;
+    }
+    return result;
   }
+
+
+
+
+
+
+
 }
