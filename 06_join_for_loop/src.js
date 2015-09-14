7fbeeabb54d5e04f494f5b86c8d8de8c796b2c49
Micah Bell <micahmbell@gmail.com>
2015-09-14 10:56:25 -0600

    06_join_for_loop complete

--- 06_join_for_loop/src.js
+++ 06_join_for_loop/src.js
@@ -4,8 +4,76 @@
 
 module.exports = {
 
-  joinWithFor: function(list) {
-    // your code here
-  }
+  joinWithFor: function(array) {
+    // var string = array.join('');
+    var string = '';
+    for (var i = 0; i < array.length; i++) {
+      string += array[i];
+    }
+    return string;
+  },
+
+  joinWithForAndIndex: function(array) {
+    var string  = '';
+    for (var i = 0; i < array.length; i++) {
+      string += array[i] + [i];
+    }
+    return string;
+  },
+
+  joinWithoutOddCharacters: function(array) {
+    var string = '';
+    if(array.length === 0) {
+      return string;
+    } else {
+      for (var i = 0; i < array.length; i++) {
+        if(i % 2 === 0) {
+          string += array[i];
+        }
+      }
+      return string;
+    }
+  },
+
+  joinWithForAndToken: function(array, token) {
+    var string = '';
+    if(array.length  === 0) {
+      return string;
+    } else {
+      for (var i = 0; i < array.length; i++) {
+        if(i === array.length - 1) {
+          string += array[i];
+        } else {
+          string += array[i] + token;
+        }
+      }
+      return string;
+    };
+  },
+
+  joinWithForAndAlternatingTokens: function(array, token, operator) {
+    var string = '';
+    if(array.length === 0) {
+      return string;
+    } else {
+      for (var i = 0; i < array.length; i++) {
+        if(i === array.length - 1) {
+          string += array[i];
+        } else if (array[i] % 2 === 1) {
+          string += array[i] + token;
+        } else if (array[i] % 2 === 0) {
+          string += array[i] + operator;
+        }
+      }
+      return string;
+    }
+  },
 
 }
+
+
+
+
+
+
+//
